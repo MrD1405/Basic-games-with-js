1d9ec0c9f3a54a883d222c3d6a4bd0b0c8142682
Dushyant <98117205+MrD1405@users.noreply.github.com>
2025-01-21 19:38:50 +0530

    Add files via upload

---
+++ 2048Game/Tile.js
@@ -0,0 +1,35 @@
+export default class Tile{
+    #tileElement
+    #x
+    #y
+    #value
+    constructor(tileContainer,value=Math.random()>.5 ?2:4){
+        this.#tileElement=document.createElement("div");
+        this.#tileElement.classList.add("tile");
+        tileContainer.append(this.#tileElement);
+        this.value=value;
+    }
+
+    set x(value){
+        this.#x=value;
+        this.#tileElement.style.setProperty('--x',value);
+    }
+    set y(value){
+        this.#y=value;
+        this.#tileElement.style.setProperty('--y',value);
+    }
+    set value(v){
+        this.#value=v;
+        this.#tileElement.textContent=this.#value;
+        const power=Math.log2(v);
+        const backgroundLightness=100-power*6;
+        this.#tileElement.style.setProperty("--background-lightness",
+            `${backgroundLightness}%`
+        )
+        this.#tileElement.style.setProperty("--text-lightness",
+            `${backgroundLightness>50 ? 10:90}%`
+        )
+    
+
+    }
+}
